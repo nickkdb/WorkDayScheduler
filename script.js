e350727b1565f0365e9b92f7963370c0988858ec
wutango <nickkdb@gmail.com>
2020-10-16 20:18:06 -0400

    cleaned up functions and added clear ability

--- script.js
+++ script.js
@@ -10,38 +10,43 @@ var three= document.getElementById("three");
 var four= document.getElementById("four");
 var five= document.getElementById("five");
 var btn = document.querySelectorAll("button");
+var clearbtn= document.getElementById("clear");
 
 var schedule = {
     sched: [nine, ten, eleven, twelve, one, two, three, four, five], 
-    times: [9, 10, 11, 12, 13, 14, 15, 18, 19]
-}
+    times: [9, 10, 11, 12, 13, 14, 15, 16, 17]
+};
 
-
+function ScheduleColor() {
     for (var i= 0; i < 9; i++) {
         if (schedule.times[i] < moment().format('H')) {
-           schedule.sched[i].style.background = "rgba(128, 128, 128, 0.432)";
+           schedule.sched[i].className = "past";
         } else if (schedule.times[i] == moment().format('H')) {
-            schedule.sched[i].style.background = "LightCoral";
+            schedule.sched[i].className = "present";
+        } else {
+            schedule.sched[i].className = "future";
         }
     }
+}
+
+function main() {
+    for (var i= 0; i < schedule.sched.length; i++) {
+        var text= schedule.sched[i];
+        var item = localStorage.getItem(text.id);
+        text.value = item;
+    }
+    
+    ScheduleColor();
+}
 
 $(btn).on("click", function() {
     var ff= document.getElementById(this.value);
     localStorage.setItem(this.value, ff.value);
 });
 
-function open() {
-    for (var i= 0; i < schedule.sched.length; i++) {
-        var text= schedule.sched[i];
-        var item = localStorage.getItem(text.id);
-        text.value = item;
-    }
-}
+$(clearbtn).on("click", function() {
+    localStorage.clear();
+    location.href= "index.html";
+});
 
-open();
-
-
-
-
-
-
+main();
